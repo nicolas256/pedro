529c31c14fd9e36334ebdb2aeec1acd15188b97d
Nicolas de Andrade <nicolasnipe@gmail.com>
2024-01-22 10:10:06 -0300

    sal

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,4 +1,6 @@
 import { server } from './server';
 
 
-server.listen(3213, () => console.log('App funcionando'));
+server.listen(process.env.PORT || 3213, () => {
+  console.log(`App rodando na porta ${process.env.PORT || 3213}`);
+});
